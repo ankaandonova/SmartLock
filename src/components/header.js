5a4009a5806040795f7fee0f5f7dcef79f3df333
ankaandonova <anka.andonova@student.hu.nl>
2021-06-22 19:39:55 +0200

    add users

--- src/components/header.js
+++ src/components/header.js
@@ -1,44 +1,44 @@
 import * as React from 'react';
 import { Appbar, Title } from 'react-native-paper';
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import {  Button, Icon , Left, Right,Body,  } from 'native-base';
+import { Button, Icon, Right, Body, } from 'native-base';
 
 
 export default Header = () => {
- 
+
   return (
     <Appbar.Header
-    theme={{
-        colors:{
-            primary: "skyblue"
-        } 
-    }}>
-    
-     
-    <Body>  
-       <Title style={styles.titel}> Smart Lock</Title>
-    </Body>
+      theme={{
+        colors: {
+          primary: "skyblue"
+        }
+      }}>
 
-     <Right> 
-      <Button transparent>
-        <Icon name='menu' />
-      </Button>
+
+      <Body>
+        <Title style={styles.titel}> Smart Lock</Title>
+      </Body>
+
+      <Right>
+        <Button transparent>
+          <Icon name='menu' color={'grey'} />
+        </Button>
       </Right>
-    
-      
+
+
     </Appbar.Header>
   );
 };
 
 
 const styles = StyleSheet.create({
-  titel:{
+  titel: {
     textAlign: "center",
     color: "white",
     fontSize: 20,
     fontWeight: 'bold',
-  
+
   },
 });
 
--- src/view/Access.js
+++ src/view/Access.js
@@ -1,48 +1,109 @@
-import React, {  useCallback } from "react";
-//import { SolidNodeClient } from 'solid-node-client';
-import {View , Text , Button} from 'react-native';
-
+import React, { useCallback } from "react";
+import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 import Header from "../components/header";
 
 export default Access = () => {
-  // const client = new SolidNodeClient();
+  const state = {
+    MyTextInput: '',
+    user: ['Jon', 'James']
+  }
 
-  // const solid_link = "https://jvwer.solidcommunity.net";
-  // const filename = "trinsic-wallet-16-6-2021-1399fdf6.txt";
-  // const foldername = "backup_credentials";
-  
-  // const account_username = "jvwer";
-  // const account_password = "kY4sfHeoia5vEBAUCDrT";
-
-
-
-  // const login = async () => {
-  //   let loginformation = await client.login({
-  //     idp : solid_link, 
-  //     username : account_username,
-  //     password : account_password,
-  //   });
-  //   return loginformation;
+  // const onChangeInput = (event) => {
+  //   setState({
+  //     MyTextInput: event
+  //   })
   // }
-  
-  // const waitForLogin = async ( loginformation ) => {
-  //   if(!loginformation.loggedIn){
-  //     setTimeout(waitForLogin( loginformation ), 400);
-  //     return;
-  //   }
-  //   console.log( loginformation );
-  // }
-  
+
+  const onAddUser = () => {
+    alert("add new user")
+    // setState(prevState => {
+    //   return {
+    //     MyTextInput: '',
+    //     users: [...prevState.users.prevState.MyTextInput]
+    //   }
+    // })
+
+  }
+
   return (
     <View>
-      <View style={{flex:1}}>
-        <Header /> 
-          <Text> access screenss
-          </Text>
+      <Header />
+      <View style={styles.body}>
+
+        <TouchableOpacity
+          onPress={onAddUser}
+          style={styles.button}
+        >
+          <Entypo name="add-user" size={28} color={'grey'} />
+        </TouchableOpacity>
+
       </View>
 
-    
+      <View style={styles.usersView}>
+        <TouchableOpacity>
+          {
+            state.user.map(item => (
+              <Text style={styles.users} key={item}> {item} </Text>
+            ))
+          }
+        </TouchableOpacity>
+      </View>
+
+      {/* <TextInput
+        value={state.MyTextInput}
+        onChangeText={onChangeInput}
+        style={styles.input}
+      /> */}
+
+
+
 
     </View>
+
+
+
+
   );
 };
+
+const styles = StyleSheet.create({
+
+
+  usersView: {
+    marginBottom: 100
+  },
+
+  users: {
+    fontSize: 20,
+    borderWidth: 0.2,
+    borderRadius: 10,
+    borderColor: 'grey',
+    padding: 10,
+    marginBottom: 20
+
+
+  },
+
+  body: {
+    marginLeft: '78%',
+    marginTop: 25,
+    height: '20%'
+  },
+
+  button: {
+    marginBottom: 350,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    borderColor: "deepskyblue",
+    backgroundColor: 'white',
+    shadowColor: 'deepskyblue',
+    elevation: 10
+  },
+
+
+
+});
